6fd82e43ae79efd1cb194a7ac2ff609c8a679636
Andriy Knysh <aknysh@users.noreply.github.com>
2019-04-10 12:33:38 -0400

    Add example usage (#4)

---
+++ examples/complete/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "${var.region}"
+}
+
+module "vpc_peering_cross_account" {
+  source    = "../../"
+  namespace = "${var.namespace}"
+  stage     = "${var.stage}"
+  name      = "${var.name}"
+
+  requester_aws_assume_role_arn             = "${var.requester_aws_assume_role_arn}"
+  requester_region                          = "${var.requester_region}"
+  requester_vpc_id                          = "${var.requester_vpc_id}"
+  requester_allow_remote_vpc_dns_resolution = "${var.requester_allow_remote_vpc_dns_resolution}"
+
+  accepter_aws_assume_role_arn             = "${var.accepter_aws_assume_role_arn}"
+  accepter_region                          = "${var.accepter_region}"
+  accepter_vpc_id                          = "${var.accepter_vpc_id}"
+  accepter_allow_remote_vpc_dns_resolution = "${var.accepter_allow_remote_vpc_dns_resolution}"
+}
---
+++ examples/complete/variables.tf
@@ -0,0 +1,67 @@
+variable "region" {
+  type        = "string"
+  description = "AWS Region"
+  default     = "us-east-1"
+}
+
+variable "namespace" {
+  type        = "string"
+  description = "Namespace (e.g. `eg` or `cp`)"
+  default     = "eg"
+}
+
+variable "stage" {
+  type        = "string"
+  description = "Stage (e.g. `prod`, `dev`, `staging`)"
+  default     = "testing"
+}
+
+variable "name" {
+  type        = "string"
+  description = "Name of the application"
+  default     = "vpc-peering"
+}
+
+variable "requester_aws_assume_role_arn" {
+  type        = "string"
+  description = "Requester AWS Assume Role ARN"
+}
+
+variable "requester_region" {
+  type        = "string"
+  description = "Requester AWS region"
+  default     = "us-west-2"
+}
+
+variable "requester_vpc_id" {
+  type        = "string"
+  description = "Requester VPC ID filter"
+}
+
+variable "requester_allow_remote_vpc_dns_resolution" {
+  type        = "string"
+  description = "Allow requester VPC to resolve public DNS hostnames to private IP addresses when queried from instances in the accepter VPC"
+  default     = "true"
+}
+
+variable "accepter_aws_assume_role_arn" {
+  type        = "string"
+  description = "Accepter AWS Assume Role ARN"
+}
+
+variable "accepter_region" {
+  type        = "string"
+  description = "Accepter AWS region"
+  default     = "us-east-1"
+}
+
+variable "accepter_vpc_id" {
+  type        = "string"
+  description = "Accepter VPC ID filter"
+}
+
+variable "accepter_allow_remote_vpc_dns_resolution" {
+  type        = "string"
+  description = "Allow accepter VPC to resolve public DNS hostnames to private IP addresses when queried from instances in the requester VPC"
+  default     = "true"
+}
