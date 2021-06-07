8232f990977edd85ccb5d4928017bb2de7494b4b
heathsnow <hsnow@impinj.com>
2021-06-07 14:05:40 -0700

    Update `complete` test
    - Remove `requester_aws_assume_role_arn`
    - Remove `requester_region`

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -5,13 +5,9 @@ provider "aws" {
 module "vpc_peering_cross_account" {
   source = "../../"
 
-  requester_aws_assume_role_arn             = var.requester_aws_assume_role_arn
-  requester_region                          = var.requester_region
   requester_vpc_id                          = var.requester_vpc_id
   requester_allow_remote_vpc_dns_resolution = var.requester_allow_remote_vpc_dns_resolution
 
-  accepter_aws_assume_role_arn             = var.accepter_aws_assume_role_arn
-  accepter_region                          = var.accepter_region
   accepter_vpc_id                          = var.accepter_vpc_id
   accepter_allow_remote_vpc_dns_resolution = var.accepter_allow_remote_vpc_dns_resolution
 
