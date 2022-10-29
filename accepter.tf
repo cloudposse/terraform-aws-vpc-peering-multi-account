1f3712bc76422346b871f1a8aef834fe66550256
Nitin Goyal <nitingoyal.dev@gmail.com>
2022-10-29 13:06:48 +0530

    Update accepter.tf

--- accepter.tf
+++ accepter.tf
@@ -109,8 +109,8 @@ resource "aws_route" "accepter" {
   count                       = local.enabled ? local.accepter_aws_route_table_ids_count * local.requester_cidr_block_associations_count : 0
   provider                    = aws.accepter
   route_table_id              = local.accepter_aws_route_table_ids[floor(count.index / local.requester_cidr_block_associations_count)]
-  destination_cidr_block      = can(regex("::", local.requester_cidr_block_associations[count.index % local.requester_cidr_block_associations_count]["cidr_block"])) ? "" : local.requester_cidr_block_associations[count.index % local.requester_cidr_block_associations_count]["cidr_block"]
-  destination_ipv6_cidr_block = can(regex("::", local.requester_cidr_block_associations[count.index % local.requester_cidr_block_associations_count]["cidr_block"])) ? local.requester_cidr_block_associations[count.index % local.requester_cidr_block_associations_count]["cidr_block"] : ""
+  destination_cidr_block      = can(regex("::", local.requester_cidr_block_associations[count.index % local.requester_cidr_block_associations_count]["cidr_block"])) ? null : local.requester_cidr_block_associations[count.index % local.requester_cidr_block_associations_count]["cidr_block"]
+  destination_ipv6_cidr_block = can(regex("::", local.requester_cidr_block_associations[count.index % local.requester_cidr_block_associations_count]["cidr_block"])) ? local.requester_cidr_block_associations[count.index % local.requester_cidr_block_associations_count]["cidr_block"] : null
   vpc_peering_connection_id   = join("", aws_vpc_peering_connection.requester.*.id)
   depends_on = [
     data.aws_route_tables.accepter,
