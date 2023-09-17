aa78d45328e723a479d5cd8b0ca76d2b90c3a14a
Max Lobur <max_lobur@outlook.com>
2023-09-18 00:54:05 +0300

    updates

--- accepter.tf
+++ accepter.tf
@@ -59,10 +59,10 @@ data "aws_subnets" "accepter" {
 }
 
 locals {
-  accepter_subnet_ids       = local.accepter_enabled ? data.aws_subnets.accepter[0].ids : []
-  accepter_vpc_id           = join("", data.aws_vpc.accepter[*].id)
-  accepter_account_id       = join("", data.aws_caller_identity.accepter[*].account_id)
-  accepter_region           = join("", data.aws_region.accepter[*].name)
+  accepter_subnet_ids = local.accepter_enabled ? data.aws_subnets.accepter[0].ids : []
+  accepter_vpc_id     = join("", data.aws_vpc.accepter[*].id)
+  accepter_account_id = join("", data.aws_caller_identity.accepter[*].account_id)
+  accepter_region     = join("", data.aws_region.accepter[*].name)
 }
 
 data "aws_route_tables" "accepter" {
