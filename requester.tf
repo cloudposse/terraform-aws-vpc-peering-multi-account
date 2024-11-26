979ac955c50406c76e669e229db6c6f6c6870059
Nathan Baulch <nathan.baulch@gmail.com>
2024-11-27 09:53:50 +1100

    Better support for omitting role_arn

--- requester.tf
+++ requester.tf
@@ -13,6 +13,7 @@ variable "requester_aws_access_key" {
 variable "requester_aws_assume_role_arn" {
   description = "Requester AWS Assume Role ARN"
   type        = string
+  default     = null
 }
 
 variable "requester_aws_secret_key" {
@@ -64,7 +65,7 @@ provider "aws" {
   skip_metadata_api_check = var.skip_metadata_api_check
 
   dynamic "assume_role" {
-    for_each = var.requester_aws_assume_role_arn != "" ? ["true"] : []
+    for_each = coalesce(var.requester_aws_assume_role_arn, "") != "" ? ["true"] : []
     content {
       role_arn = var.requester_aws_assume_role_arn
     }
